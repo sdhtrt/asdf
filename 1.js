c61462bf4cf885d3b5a8e622291fc054723f1225
sdhtrt <86913831+sdhtrt@users.noreply.github.com>
2021-08-03 17:08:26 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,10 @@
+'use strict';
+alert(Infinity/3);
+// let number = 9823648759210379040348653754346463n;
+// alert(number)
+let userName = prompt('кто ты?', 'лютый');
+let adminName = userName;
+alert(`Вы - ${adminName}`);
+let num = 1
+num = String(num);
+alert(typeof(num));
