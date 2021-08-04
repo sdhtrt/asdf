9dec2852fcd215b5a9eda83a2d6ee0185961a8d7
Alexey <alexey.brysh@gmail.com>
2021-08-04 22:11:18 +0300

    different architecture

---
+++ js/1.js
@@ -0,0 +1,9 @@
+'use strict';
+alert(Infinity/3);
+// let number = 9823648759210379040348653754346463n;
+// alert(number)
+let userName = prompt('кто ты?', 'лютый');
+alert(`Вы - ${userName}`);
+let num = 1
+num = String(num);
+alert(typeof(num));
