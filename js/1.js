3b674e8aa03ee348c29b59420b835f968091d28c
Alexey <alexey.brysh@gmail.com>
2021-08-12 23:52:27 +0300

    2 pages done

--- js/1.js
+++ js/1.js
@@ -1,9 +1,3 @@
 'use strict';
-alert(Infinity/3);
-// let number = 9823648759210379040348653754346463n;
-// alert(number)
-let userName = prompt('кто ты?', 'лютый');
+let userName = prompt('кто ты?', '');
 alert(`Вы - ${userName}`);
-let num = 1
-num = String(num);
-alert(typeof(num));
